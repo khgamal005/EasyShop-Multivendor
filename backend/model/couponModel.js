986e5c384bcd2362e1857b94de399526ebba4806
khgamal005 <khgamal005@gmail.com>
2025-05-30 06:00:11 +0300

    update couponemodel

--- backend/model/couponModel.js
+++ backend/model/couponModel.js
@@ -1,23 +1,42 @@
 const mongoose = require('mongoose');
 
-const couponSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      trim: true,
-      required: [true, 'Coupon name required'],
-      unique: true,
-    },
-    expire: {
-      type: Date,
-      required: [true, 'Coupon expire time required'],
-    },
-    discount: {
-      type: Number,
-      required: [true, 'Coupon discount value required'],
-    },
+const couponSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please enter your coupon code name!"],
+    unique: true,
   },
-  { timestamps: true }
-);
+  value: {
+    type: Number,
+    required: true,
+  },
+  isFlat: {
+    type: Boolean,
+    default: false, // sellers default to percentage
+  },
+  minAmount: {
+    type: Number,
+  },
+  maxAmount: {
+    type: Number,
+  },
+  shopId: {
+    type: String,
+    required: false,
+    default: null,
+  },
+  selectedProduct: {
+    type: String,
+  },
+  createdBy: {
+    type: String,
+    enum: ["admin", "seller"],
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  }
+});
 
 module.exports = mongoose.model('Coupon', couponSchema);
